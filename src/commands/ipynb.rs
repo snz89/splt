930b47a0e921134c7bf00f1a0db0a4b571c108c0
snz <seanzfx@yandex.ru>
2026-08-02 22:17:02 +0300

    remove unnecessary dynamic dispatch in split and ipynb commands

--- src/commands/ipynb.rs
+++ src/commands/ipynb.rs
@@ -1,44 +1,106 @@
 use std::{
     fs::File,
     io::{self, BufRead, BufReader, BufWriter, IsTerminal, Write},
+    path::Path,
 };
 
 use serde::Deserialize;
 
 use crate::cli::IpynbConvertArgs;
-use anyhow::{Context, Result, bail};
+use anyhow::{Context, bail};
 
 use crate::errors::TerminalInputNotSupportedError;
 
-pub fn handle(args: IpynbConvertArgs) -> Result<()> {
+#[derive(Deserialize)]
+struct Notebook {
+    cells: Vec<Cell>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Cell {
+    cell_type: String,
+    source: Vec<String>,
+}
+
+pub fn handle(args: IpynbConvertArgs) -> anyhow::Result<()> {
     if args.input_path.is_none() && io::stdin().is_terminal() {
         bail!(TerminalInputNotSupportedError);
     }
 
-    let reader: Box<dyn BufRead> = match args.input_path {
-        Some(path) => {
-            let file = File::open(path).context("Cannot open input file")?;
-            Box::new(BufReader::new(file))
-        }
-        None => Box::new(io::stdin().lock()),
-    };
+    let output_path = args.output_path.as_deref();
 
+    if let Some(path) = args.input_path {
+        let file = File::open(path).context("Cannot open input file")?;
+        let reader = BufReader::new(file);
+        handle_output(
+            output_path,
+            args.cell_start_index,
+            args.cell_headers_enabled,
+            &args.cell_prefix,
+            reader,
+        )
+    } else {
+        let reader = io::stdin().lock();
+        handle_output(
+            output_path,
+            args.cell_start_index,
+            args.cell_headers_enabled,
+            &args.cell_prefix,
+            reader,
+        )
+    }
+}
+
+fn handle_output<R>(
+    output_path: Option<&Path>,
+    cell_start_index: u32,
+    cell_headers_enabled: bool,
+    cell_prefix: &str,
+    reader: R,
+) -> anyhow::Result<()>
+where
+    R: BufRead,
+{
+    if let Some(path) = output_path {
+        let file = File::create(path).context("Cannot create output file")?;
+        let mut writer = BufWriter::new(file);
+        handle_streams(
+            cell_start_index,
+            cell_headers_enabled,
+            cell_prefix,
+            reader,
+            &mut writer,
+        )
+    } else {
+        let mut writer = io::stdout().lock();
+        handle_streams(
+            cell_start_index,
+            cell_headers_enabled,
+            cell_prefix,
+            reader,
+            &mut writer,
+        )
+    }
+}
+
+fn handle_streams<R, W>(
+    cell_start_index: u32,
+    cell_headers_enabled: bool,
+    cell_prefix: &str,
+    reader: R,
+    writer: &mut W,
+) -> anyhow::Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
     let notebook: Notebook = serde_json::from_reader(reader)?;
-
-    let mut writer: Box<dyn Write> = match args.output_path {
-        Some(path) => {
-            let file = File::create(path).context("Cannot create output file")?;
-            Box::new(BufWriter::new(file))
-        }
-        None => Box::new(io::stdout().lock()),
-    };
-
     let code_cells = notebook.cells.into_iter().filter(|c| c.cell_type == "code");
 
-    let mut cell_index = args.cell_start_index;
+    let mut cell_index = cell_start_index;
     for cell in code_cells {
-        if args.cell_headers_enabled {
-            writeln!(writer, "# {}{}", args.cell_prefix, cell_index)?;
+        if cell_headers_enabled {
+            writeln!(writer, "# {cell_prefix}{cell_index}")?;
             cell_index += 1;
         }
 
@@ -51,15 +113,4 @@ pub fn handle(args: IpynbConvertArgs) -> Result<()> {
     writer.flush()?;
 
     Ok(())
-}
-
-#[derive(Deserialize)]
-struct Notebook {
-    cells: Vec<Cell>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Cell {
-    cell_type: String,
-    source: Vec<String>,
 }
--- src/commands/split.rs
+++ src/commands/split.rs
@@ -4,7 +4,7 @@ use std::{
     path::Path,
 };
 
-use anyhow::{Context, Ok, Result, bail};
+use anyhow::{Context, bail};
 
 use crate::{
     batching::{Batch, BatchesIterator},
@@ -12,30 +12,45 @@ use crate::{
     errors::TerminalInputNotSupportedError,
 };
 
-pub fn handle(args: SplitArgs) -> Result<()> {
+pub fn handle(args: SplitArgs) -> anyhow::Result<()> {
     if args.input_path.is_none() && io::stdin().is_terminal() {
         bail!(TerminalInputNotSupportedError);
     }
 
-    let reader: Box<dyn BufRead> = match args.input_path {
-        Some(path) => {
-            let file = File::open(path).context("Cannot open input file")?;
-            Box::new(BufReader::new(file))
-        }
-        None => Box::new(BufReader::new(io::stdin().lock())),
-    };
-    let lines = reader.lines().map_while(Result::ok);
-    let batches = BatchesIterator::new(
-        lines.into_iter(),
-        args.max_line_length,
-        args.weights.into_iter(),
-    )?;
+    let max_line_length = args.max_line_length;
+    let weights_iter = args.weights.into_iter();
+    let output_dir = &args.output_dir;
 
-    write_batches(batches, &args.output_dir)?;
+    if let Some(path) = args.input_path {
+        let file = File::open(path).context("Cannot open input file")?;
+        let reader = BufReader::new(file);
+        split(reader, max_line_length, weights_iter, output_dir)
+    } else {
+        let reader = io::stdin().lock();
+        split(reader, max_line_length, weights_iter, output_dir)
+    }
+}
+
+fn split<R>(
+    reader: R,
+    max_line_length: usize,
+    weights_iter: impl Iterator<Item = usize>,
+    output_dir: &Path,
+) -> anyhow::Result<()>
+where
+    R: BufRead,
+{
+    let lines = reader.lines().map_while(Result::ok);
+    let batches = BatchesIterator::new(lines, max_line_length, weights_iter)?;
+
+    write_batches(batches, output_dir)?;
     Ok(())
 }
 
-fn write_batches(batches: impl Iterator<Item = Batch>, output_dir: &Path) -> io::Result<()> {
+fn write_batches<I>(batches: I, output_dir: &Path) -> io::Result<()>
+where
+    I: Iterator<Item = Batch>,
+{
     fs::create_dir_all(output_dir)?;
 
     for (batch_id, batch) in batches.enumerate() {
